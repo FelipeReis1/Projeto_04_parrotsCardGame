ae6a85350b3aca24a83dad43f5baa7b64e81187b
FelipeReis1 <feliperg15@yahoo.com.br>
2022-10-23 21:14:46 -0300

    virar cartas e comparar cartas funcionando, ajustes no css

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let quantidadeCartasSelecionada;
-//let numeroJogadas = 0;
+let numeroJogadas = 0;
+let primeiraCarta = "";
+let segundaCarta = "";
 let meuContainerCartas = document.querySelector(".containerCartas");
 let meusGifs = [
   "./imagens/bobrossparrot.gif",
@@ -11,27 +13,66 @@ let meusGifs = [
   "./imagens/unicornparrot.gif",
 ];
 
-while (
-  quantidadeCartasSelecionada < 4 ||
-  quantidadeCartasSelecionada > 14 ||
-  quantidadeCartasSelecionada % 2 !== 0
-) {
-  quantidadeCartasSelecionada = Number(prompt("Qual a quantidade de cartas?"));
+iniciar();
+function iniciar() {
+  while (
+    quantidadeCartasSelecionada < 4 ||
+    quantidadeCartasSelecionada > 14 ||
+    quantidadeCartasSelecionada % 2 !== 0
+  ) {
+    quantidadeCartasSelecionada = Number(
+      prompt("Qual a quantidade de cartas?")
+    );
+  }
+
+  function comparador() {
+    return Math.random() - 0.5;
+  }
+
+  meusGifs.sort(comparador);
+
+  let gifsIguais = meusGifs.slice(0, quantidadeCartasSelecionada / 2);
+  gifsIguais = gifsIguais.concat(gifsIguais);
+  gifsIguais.sort(comparador);
+  for (let i = 0; i < quantidadeCartasSelecionada; i++) {
+    meuContainerCartas.innerHTML += `<ul class="card" onclick ="virarCarta(this)">
+    <li class = "cartaFrente face"><img src="./imagens/front.png" /></li>
+    <li class="cartaVirada face"><img src="${gifsIguais[i]}" /></li>  
+    </ul>
+    `;
+  }
 }
 
-function comparador() {
-  return Math.random() - 0.5;
+function virarCarta(elemento) {
+  if (primeiraCarta === "") {
+    primeiraCarta = elemento;
+    elemento.classList.add("virar");
+    numeroJogadas++;
+  } else {
+    segundaCarta = elemento;
+    elemento.classList.add("virar");
+    numeroJogadas++;
+    if (primeiraCarta.innerHTML === segundaCarta.innerHTML) {
+      primeiraCarta = "";
+      segundaCarta = "";
+      if (
+        document.querySelectorAll(".virar").length ===
+        quantidadeCartasSelecionada
+      )
+        setTimeout(finalizaJogo, 1000);
+    } else {
+      setTimeout(comparaCartas, 1000);
+    }
+  }
 }
 
-meusGifs.sort(comparador);
+function comparaCartas() {
+  primeiraCarta.classList.remove("virar");
+  segundaCarta.classList.remove("virar");
+  primeiraCarta = "";
+  segundaCarta = "";
+}
 
-let gifsIguais = meusGifs.slice(0, quantidadeCartasSelecionada / 2);
-gifsIguais = gifsIguais.concat(gifsIguais);
-gifsIguais.sort(comparador);
-for (let i = 0; i < quantidadeCartasSelecionada; i++) {
-  meuContainerCartas.innerHTML += `<div class="card">
-  <div class = "cartaFrente face"><img src="./imagens/front.png" /></div>
-  <div class="cartaVirada face"><img src="${gifsIguais[i]}" /></div>  
-  </div>
-  `;
+function finalizaJogo() {
+  alert(`Parabéns, você ganhou o jogo com: ${numeroJogadas} jogadas!`);
 }
